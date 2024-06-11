04bd9e0d85423ad3bc26513a29015a59634fb8c7
mapula <mapule.6862@gmail.com>
2024-06-11 11:08:35 +0200

    java basics

---
+++ operators.js
@@ -0,0 +1,46 @@
+let k =2
+let j =10.5
+let add=k+j
+let sub=j-k
+let mult=k*j
+let mod=k%j
+let exp=k**j
+console.log(add)
+console.log(sub)
+console.log(mult)
+console.log(mod)
+console.log(exp)
+
+let m=10
+let L=2
+
+console.log(m>L)
+console.log(m<L)
+console.log(m<=L)
+console.log(m>=L)
+
+let x=8
+let y =12
+console.log(x>y)
+console.log(x<=y)
+console.log(x===y)
+console.log(x!=y)
+
+let a=5
+let b=2
+console.log(a>b && b>a)
+console.log(a>b ||b>a)
+console.log(!a)
+
+let p=10
+
+p+=2
+console.log(p)
+p-=1
+console.log(p)
+p*=3
+console.log(p)
+p/=2
+console.log(p)
+p%=9
+console.log(p)
---
+++ variables.js
@@ -0,0 +1,21 @@
+let name='mapula'
+let age= "22"
+let istudent="true"
+console.log(name + age + istudent)
+
+var favoritecolour= "black"
+var favoritecolour="white"
+console.log(favoritecolour)
+
+
+let num1=8
+let num2=2
+let sum=num1+num2
+let diff= num1-num2
+let product=num1*num2
+let quotient=num1/num2
+console.log(sum)
+console.log(diff)
+console.log(product)
+console.log(quotient)
+
