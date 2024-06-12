edd98a5d7522f7561dea7f41caa6037595ad7377
mapula <mapule.6862@gmail.com>
2024-06-12 15:51:27 +0200

    looops4

--- statements.js
+++ statements.js
@@ -67,7 +67,7 @@ for (let i = 1; i <= 100; i++) {
 }
 console.log(sum1);
 
-const numbers = [1, 2, 3, 4, 5];
+let numbers = [1, 2, 3, 4, 5];
 
 for (i = 0; i < numbers.length; i++) console.log(numbers[i]);
 
@@ -80,3 +80,102 @@ for (i = 0; i < numbers1.length; i++) {
   }
 }
 console.log(biggest);
+
+
+var i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+let s = 2;
+while (s<= 20) {
+  console.log(s);
+  s+=2;
+}
+
+
+let u= 1;
+let sum3 = 0;
+
+while (u <= 100) {
+  sum3 +=u;
+  u++;
+}
+
+console.log(sum3);
+
+
+let w= 1;
+
+while (w < 50) {
+  if (w % 5 === 0) {
+    console.log(w);
+  }
+  w++;
+}
+
+
+let f = 1;
+
+do {
+  console.log(f);
+  f++;
+} while (f <= 10);
+
+
+let v= 1;
+let sum8= 0;
+
+do {
+  sum8+=v;
+  v++;
+} while (v<=100);
+
+console.log(sum8);
+
+let userInput;
+
+do {
+  userInput = parseInt(prompt("Please enter a number greater than 10:"));
+  if (userInput > 10) {
+    console.log("well done:", userInput);
+    break;
+  } else if (isNaN(userInput) || userInput <= 10) {
+    console.log("Error:number greater than 10.");
+  }
+} while (true);
+
+
+
+
+
+
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+let userGuess;5
+let attempts = 0;
+
+do {
+  userGuess = parseInt(prompt("Guess a number between 1 and 10:"));
+  attempts++;
+
+  if (userGuess === randomNumber) {
+    console.log(`Congratulations!${randomNumber} in ${attempts} attempts.`);
+  } else if (userGuess < randomNumber) {
+    console.log("is too low");
+  } else {
+    console.log("drop it a bit");
+  }
+} while (userGuess !== randomNumber);
+
+
+
+
+
+
+
+
+
+
+
