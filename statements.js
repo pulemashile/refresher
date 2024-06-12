daa5785cfec795b2a3f3b266b4c6eba23482f653
mapula <mapule.6862@gmail.com>
2024-06-12 13:10:24 +0200

    loops

--- statements.js
+++ statements.js
@@ -8,6 +8,7 @@ else if(temperature>=16&&temperature<=25)
 else if (temperature>25)
     console.log("its warm")
 
+
 let temp=25
 let condition;
 
@@ -30,24 +31,98 @@ switch (true) {
 console.log("temperature:"+" "+condition)
 
 
-let num=5
-let divisibility;
 
-switch (true) {
-    case (num%2 && num%3):
-        divisibility="divisible by both";
-        break;
-    case num/2 :
-        divisibility="divisible by 2";
-        break
-    case num%3  :
-            divisibility="divisible by 3";
-        break
-        case !(num%2 && num%3):
-            divisibility="not divisible by 2 or 3";
-    
-        break;
+
+
+
+//let X=90
+//if (X%2){
+   // console.log("its divisible by 3")
+//}else if (X%3)
+    //console.log("divisible by 2");
+//else if(X%2 && X%3)
+    //console.log("its not divisible by either")
+//else if (!X%2 &&!X%3)
+    //console.log("divsible by both")
+
+
+
+    function devisibleBy(num) {
+
+        let devisible = "";
+      switch(true){
+       case num % 2 == 0 && num % 3 == 0:
+      devisible = 'Divible by 2 and 3'
+       break;
+       case num % 2 == 0:
+       devisible = 'Divible by 2'
+      break;
+       case num % 3 == 0:
+      devisible = 'Divible by 3'
+      break;
+      default:
+      devisible = 'Number is devisible by none'
+      
+        }
+      
+      console.log(devisible)
 }
 
-console.log("divisibility:"+" "+divisibility)
+devisibleBy(8)
+
+
+for(let i=1;i<=10;i++)
+    console.log(i)
+ 
+ 
+ for(let i=2;i<=20;i++)
+    if(i%3 !==0)console.log(i)
+ 
+ 
+     var sum1=0
+     for(let i=1;i<=100;i++){  
+         sum1=sum1+i
+     }
+     console.log(sum1)
+     
+     
+
+
+     const numbers=[1,2,3,4,5]
+     
+      for(i=0;i<numbers.length;i++)
+        console.log(numbers[i])
+
+       var numbers1=[3,7,2,5,10,6]
+       var biggest=0
+
+       for(i=0;i<numbers1.length;i++){
+        if (biggest<numbers1[i]){
+            biggest=numbers1[i]
+        }
+       }
+        console.log(biggest)
+
+    let i=0
+    while(i<10){
+        console.log(i2)
+        i++;
+    }
+
+
+  
+        
+     
+
+     
+
+
+
+      
+      
+      
+      
+      
+      
+   
 
